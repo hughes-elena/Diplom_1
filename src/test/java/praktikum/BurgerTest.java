bcfdea364be0bc5e6df4abfab50e4c0280f7d11b
Elena Hughes <elena.f.hughes@yandex.com>
2025-06-12 11:56:35 +0200

    Внесла изменения в pom.xml и тесты разделены так, чтобы проверяли только одно условие

--- src/test/java/praktikum/BurgerTest.java
+++ src/test/java/praktikum/BurgerTest.java
@@ -15,17 +15,40 @@ public class BurgerTest {
     @Mock
     private Bun bun; // мокаем булки
     @Mock
-    private Ingredient ingredient0;
+    private Ingredient dinosaurFillingIngredient;
     @Mock
-    private Ingredient ingredient1;
+    private Ingredient chiliSauceIngredient;
     @Mock
-    private Ingredient ingredient2;
+    private Ingredient cutletFillingIngredient;
 
     private Burger burger; // что будем тестить
 
     @Before
     public void makeBurger() {
         burger = new Burger();
+    }
+
+
+    // Метод для настройки бургера с булочкой и ингредиентами
+    private void setupBurgerWithBunAndIngredients() {
+        when(bun.getName()).thenReturn("red bun");
+        when(bun.getPrice()).thenReturn(300F);
+        burger.setBuns(bun);
+
+        when(dinosaurFillingIngredient.getName()).thenReturn("dinosaur");
+        when(dinosaurFillingIngredient.getType()).thenReturn(IngredientType.FILLING);
+        when(dinosaurFillingIngredient.getPrice()).thenReturn(200F);
+        burger.addIngredient(dinosaurFillingIngredient);
+
+        when(chiliSauceIngredient.getName()).thenReturn("chili sauce");
+        when(chiliSauceIngredient.getType()).thenReturn(IngredientType.SAUCE);
+        when(chiliSauceIngredient.getPrice()).thenReturn(300F);
+        burger.addIngredient(chiliSauceIngredient);
+
+        when(cutletFillingIngredient.getName()).thenReturn("cutlet");
+        when(cutletFillingIngredient.getType()).thenReturn(IngredientType.FILLING);
+        when(cutletFillingIngredient.getPrice()).thenReturn(100F);
+        burger.addIngredient(cutletFillingIngredient);
     }
 
     @Test //тест на булочку
@@ -42,107 +65,144 @@ public class BurgerTest {
     }
 
     @Test //тест на добавление ингред
-    public void addIngredientTest() {
-        burger.addIngredient(ingredient0);
+    public void addIngredientShouldIncreaseIngredientCountTest() {
+        burger.addIngredient(dinosaurFillingIngredient);
         assertEquals("В бургер добавлен один ингредиент", 1, burger.ingredients.size());
-        assertTrue("Список должен содержать добавленный ингредиент", burger.ingredients.contains(ingredient0));
+    }
+
+    @Test
+    public void addIngredientShouldAddCorrectIngredient() {
+        burger.addIngredient(dinosaurFillingIngredient);
+        assertTrue("Список должен содержать добавленный ингредиент", burger.ingredients.contains(dinosaurFillingIngredient));
     }
 
     @Test //тест на удаление ингредиента
-    public void removeIngredientTest() {
+    public void removeIngredientShouldDecreaseIngredientsCount() {
         //добавляем ингредиенты
-        burger.addIngredient(ingredient0);
-        burger.addIngredient(ingredient1);
-        burger.addIngredient(ingredient2);
+        burger.addIngredient(dinosaurFillingIngredient);
+        burger.addIngredient(chiliSauceIngredient);
+        burger.addIngredient(cutletFillingIngredient);
 
-//удаляем первый
+        //удаляем первый
         burger.removeIngredient(0);
 
         // проверяем, что осталось 2 ингр
         assertEquals("После удаления должно остаться 2 ингредиента", 2, burger.ingredients.size());
+    }
+
+    @Test //тест, что удалился верный ингредиент
+    public void removeIngredientShouldRemoveCorrectIngredient() {
+        //добавляем ингредиенты
+        burger.addIngredient(dinosaurFillingIngredient);
+        burger.addIngredient(chiliSauceIngredient);
+        burger.addIngredient(cutletFillingIngredient);
+
+        //удаляем первый
+        burger.removeIngredient(0);
 
         // Проверяем, что ингр(0) удалился
-        assertFalse("Удаленный ингредиент все еще присутствует", burger.ingredients.contains(ingredient0));
-
-        // проверяем, что ингр1 и 2 остались
-        assertTrue("Ингредиент 1 должен остаться", burger.ingredients.contains(ingredient1));
-        assertTrue("Ингредиент 2 должен остаться", burger.ingredients.contains(ingredient2));
+        assertFalse("Удаленный ингредиент все еще присутствует", burger.ingredients.contains(dinosaurFillingIngredient));
     }
 
-    @Test //тест на изменение позиций ингред
-    public void moveIngredientTest() {
-        //добавляем 3 игред
-        burger.addIngredient(ingredient0);
-        burger.addIngredient(ingredient1);
-        burger.addIngredient(ingredient2);
-        //перемещаем первый на последнее место 0 -> 2
-        burger.moveIngredient(0, 2);
-        //проверяем новый порядок
-        assertEquals("На позиции 0 стоит ingredient1", ingredient1, burger.ingredients.get(0));
-        assertEquals("На позиции 1 стоит ingredient2", ingredient2, burger.ingredients.get(1));
-        assertEquals("На позиции 2 стоит ingredient0", ingredient0, burger.ingredients.get(2));
+    @Test //тест, что остальные игредиенты остались
+    public void removeIngredientShouldKeepOtherIngredients() {
+        burger.addIngredient(dinosaurFillingIngredient);
+        burger.addIngredient(chiliSauceIngredient);
+        burger.addIngredient(cutletFillingIngredient);
+
+        burger.removeIngredient(0);
+    // проверяем, что ингр1 и 2 остались
+    assertTrue("Ингредиент 1 должен остаться",burger.ingredients.contains(chiliSauceIngredient));
+    assertTrue("Ингредиент 2 должен остаться",burger.ingredients.contains(cutletFillingIngredient));
+}
+
+@Test //тест на изменение позиций ингред dinosaur на последнее место
+public void moveIngredientShouldChangeOrderPositionTest() {
+    //добавляем 3 игред
+    burger.addIngredient(dinosaurFillingIngredient);
+    burger.addIngredient(chiliSauceIngredient);
+    burger.addIngredient(cutletFillingIngredient);
+    //перемещаем первый на последнее место 0 -> 2
+    burger.moveIngredient(0, 2);
+    //проверяем новый порядок
+    assertEquals("На позиции 0 стоит chiliSauceIngredient", chiliSauceIngredient, burger.ingredients.get(0));
+}
+
+//Тесты расчета цены
+@Test
+//тест расчет цены бургера. Булочка считается за каждую отдельно(верхняя и нижняя = 2) Цена будет=булка*2+ингр1+ингр2+инг3
+public void getPriceShouldCalculateCorrectly() {
+    //добавляем булочку
+    burger.setBuns(bun);
+    //добавляем ингредиенты
+    burger.addIngredient(dinosaurFillingIngredient);
+    burger.addIngredient(chiliSauceIngredient);
+    burger.addIngredient(cutletFillingIngredient);
+
+    //настраиваем моки и даем цену: булочка = 50 флоат, ингр0 = 35 флоат, ингр1 = 15 флоат, ингр2 = 20 флоат: 50*2+35+15+20=170флоат
+    when(bun.getPrice()).thenReturn(50.0F);
+    when(dinosaurFillingIngredient.getPrice()).thenReturn(35.0F);
+    when(chiliSauceIngredient.getPrice()).thenReturn(25.0F);
+    when(cutletFillingIngredient.getPrice()).thenReturn(20.0F);
+
+    float expectedPrice = 50.0F * 2 + 35.0F + 25.0F + 20.0F;
+
+    //Проверяем расчет
+    assertEquals("Цена рассчитана неверно", expectedPrice, burger.getPrice(), 1.0F);
+}
+
+@Test //тест на получение чека
+public void getReceiptShouldContainBunName() {
+
+    // Настроим бургер
+    setupBurgerWithBunAndIngredients();
+
+    // Получаем чек
+    String receipt = burger.getReceipt();
+
+    // Проверяем, что чек содержит всё нужное
+    assertTrue("Чек должен содержать название булочки", receipt.contains("red bun"));
+}
+
+    @Test
+    public void getReceiptShouldContainIngredientNameDinosaur() {
+        // Настроим бургер
+        setupBurgerWithBunAndIngredients();
+
+        String receipt = burger.getReceipt();
+        assertTrue("Чек должен содержать название ингредиента", receipt.contains("dinosaur"));
     }
 
-    @Test //тест расчет цены бургера. Булочка считается за каждую отдельно(верхняя и нижняя = 2) Цена будет=булка*2+ингр1+ингр2+инг3
-    public void getPriceTest() {
-        //добавляем булочку
-        burger.setBuns(bun);
-        //добавляем ингредиенты
-        burger.addIngredient(ingredient0);
-        burger.addIngredient(ingredient1);
-        burger.addIngredient(ingredient2);
+    @Test
+    public void getReceiptShouldContainIngredientNameChiliSauce() {
+        // Настроим бургер
+        setupBurgerWithBunAndIngredients();
 
-        //настраиваем моки и даем цену: булочка = 50 флоат, ингр0 = 35 флоат, ингр1 = 15 флоат, ингр2 = 20 флоат: 50*2+35+15+20=170флоат
-        when(bun.getPrice()).thenReturn(50.0F);
-        when(ingredient0.getPrice()).thenReturn(35.0F);
-        when(ingredient1.getPrice()).thenReturn(25.0F);
-        when(ingredient2.getPrice()).thenReturn(20.0F);
-
-        float expectedPrice = 50.0F * 2 + 35.0F + 25.0F + 20.0F;
-
-        //Проверяем расчет
-        assertEquals("Цена рассчитана неверно", expectedPrice, burger.getPrice(), 1.0F);
+        String receipt = burger.getReceipt();
+        assertTrue("Чек должен содержать тип ингредиента (sauce)", receipt.contains("chili sauce"));
     }
 
-    @Test //тест на получение чека
-    public void getReceiptTest() {
+    @Test
+    public void getReceiptShouldContainIngredientNameCutlet() {
+        // Настроим бургер
+        setupBurgerWithBunAndIngredients();
 
-        // Настроим булочку
-        when(bun.getName()).thenReturn("red bun");
-        when(bun.getPrice()).thenReturn(300F);
-        burger.setBuns(bun);
+        String receipt = burger.getReceipt();
+        assertTrue("Чек должен содержать название ингредиента", receipt.contains("cutlet"));
+    }
 
-        // Настроим ингредиент FILLING
-        when(ingredient0.getName()).thenReturn("dinosaur");
-        when(ingredient0.getType()).thenReturn(IngredientType.FILLING);
-        when(ingredient0.getPrice()).thenReturn(200F);
-        burger.addIngredient(ingredient0);
+    @Test
+    public void getReceiptShouldContainTotalPrice() {
+    //Настроим бургер
+        setupBurgerWithBunAndIngredients();
 
-        // Настроим ингредиент SAUCE
-        when(ingredient1.getName()).thenReturn("chili sauce");
-        when(ingredient1.getType()).thenReturn(IngredientType.SAUCE);
-        when(ingredient1.getPrice()).thenReturn(300F);
-        burger.addIngredient(ingredient1);
-
-        // Настроим ингредиент FILLING
-        when(ingredient2.getName()).thenReturn("cutlet");
-        when(ingredient2.getType()).thenReturn(IngredientType.FILLING);
-        when(ingredient2.getPrice()).thenReturn(100F);
-        burger.addIngredient(ingredient2);
-
-        float expectedPrice = 300F * 2 + 200F + 300F + 100F;
-        String expectedPriceString = String.format("Price: %f", expectedPrice); //%f — формат для вещественного (дробного) числа (float или double)
-
-        // Получаем чек
         String receipt = burger.getReceipt();
 
-        // Проверяем, что чек содержит всё нужное
-        assertTrue("Чек должен содержать название булочки", receipt.contains("red bun"));
-        assertTrue("Чек должен содержать название ингредиента", receipt.contains("dinosaur"));
-        assertTrue("Чек должен содержать тип ингредиента (sauce)", receipt.contains("chili sauce"));
-        assertTrue("Чек должен содержать название ингредиента", receipt.contains("cutlet"));
-        assertTrue("Чек должен содержать итоговую цену", receipt.contains(expectedPriceString));
-    }
+    float expectedPrice = 300F * 2 + 200F + 300F + 100F;
+    String expectedPriceString = String.format("Price: %f", expectedPrice); //%f — формат для вещественного (дробного) числа (float или double)
+
+    assertTrue("Чек должен содержать итоговую цену", receipt.contains(expectedPriceString));
+}
 
 
 }
